c2adec09dbe628f4995c9bde89e838e01b30bd74
David Porter <david.porter@daf.qld.gov.au>
2021-06-28 14:59:07 +1000

    nav file

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
 module.exports = config => {
 
-  config.addPassthroughCopy("src/META-INF")
-  config.addPassthroughCopy("src/acim.css")
-  config.addPassthroughCopy("src/metadata.opf")
-  config.addPassthroughCopy("src/mimetype")
-  config.addPassthroughCopy("src/toc.ncx")
-  config.addPassthroughCopy("src/endnotes.xhtml")
+	config.addPassthroughCopy("src/META-INF")
+	config.addPassthroughCopy("src/acim.css")
+	config.addPassthroughCopy("src/metadata.opf")
+	config.addPassthroughCopy("src/mimetype")
+	config.addPassthroughCopy("src/nav.xhtml")
 
-  return {
 
-    dir: {
-      input: "src",
-      output: "dist"
-    }
-  }
+	return {
+
+		dir: {
+			input: "src",
+			output: "dist"
+		}
+	}
 }
